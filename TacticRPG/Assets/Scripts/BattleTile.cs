d19c3f1815cfdf2c4f7328a3635413c20dc4224f
RevanVI <revan0603@gmail.com>
2020-03-16 00:05:49 +0300

    init done

---
+++ TacticRPG/Assets/Scripts/BattleTile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "BattleTile", menuName = "Tiles/BattleTile")]
+public class BattleTile : Tile
+{
+    public bool IsBlocked;
+
+#if UNITY_EDITOR
+    
+#endif
+}
