6650005a59268f30292c9181252ea72412fff815
RevanVI <47805713+RevanVI@users.noreply.github.com>
2020-05-28 16:32:45 +0300

    update behaviours

---
+++ Assets/Scripts/AI/Considerations/AmmoStatus.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AmmoStatus", menuName = "UtilityAI/AmmoStatus", order = 101)]
+public class AmmoStatus : ConsiderationBase
+{
+    public override float Score(ContextBase context)
+    {
+        Character currentCharacter = context.Provider.GetControlledCharacter();
+        if (currentCharacter.Properties.MaxMissiles == 0)
+            return 0f;
+        return ((float)currentCharacter.Properties.CurrentMissiles) / currentCharacter.Properties.MaxMissiles;
+    }
+}
